7f620f0bf209c04017905be232460930c578cdb7
Luiz Guilherme Mariano da Cruz <37917447+zBatataXDz@users.noreply.github.com>
2024-01-29 05:11:13 -0300

    Blocks: Fixed Wood Properties

--- src/main/java/net/creeperhost/chickens/block/BlockHenhouse.java
+++ src/main/java/net/creeperhost/chickens/block/BlockHenhouse.java
@@ -33,6 +33,7 @@ public class BlockHenhouse extends BaseEntityBlock
 
     public BlockHenhouse()
     {
+        // teste
         super(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD).strength(2.0F));
         this.registerDefaultState(getStateDefinition().any().setValue(FACING, Direction.NORTH));
     }
